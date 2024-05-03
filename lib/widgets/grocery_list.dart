691380d687bfd4f8d3f06e827a2e84d2b5c00b00
karim <karim.tarek96@yahoo.com>
2024-05-03 17:52:12 +0300

    added items to populate in the main screen - added removing items functionality using the dissmissable widget

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,16 +10,74 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
+
+    if (newItem == null) {
+      return;
+    } else {
+      setState(() {
+        _groceryItems.add(newItem);
+      });
+    }
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget screenSwitch = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) {
+          return Dismissible(
+            background: Container(
+              decoration: BoxDecoration(
+                  color: _groceryItems[index].category.color,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(16),
+                  ),),
+            ),
+            key: ValueKey(_groceryItems[index].id),
+            onDismissed: (direction) {
+              _removeItem(_groceryItems[index]);
+            },
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
+              ),
+              trailing: Text(
+                _groceryItems[index].quantity.toString(),
+              ),
+            ),
+          );
+        });
+
+    if (_groceryItems.isEmpty) {
+      screenSwitch = const Center(
+        child: Text(
+          'No items added yet.',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w100,
+            color: Color.fromARGB(195, 251, 250, 250),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -30,21 +88,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (ctx, index) {
-            return ListTile(
-              title: Text(groceryItems[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: groceryItems[index].category.color,
-              ),
-              trailing: Text(
-                groceryItems[index].quantity.toString(),
-              ),
-            );
-          }),
+      body: screenSwitch,
     );
   }
 }
